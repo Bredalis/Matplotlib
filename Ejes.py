4029e5e2874a34ce0473dde66c7cccc78b6d377c
PRO X <bredalisgautreaux@gmail>
2024-10-10 19:12:45 +0200

    style: 🎨 Cambio en la ortografía

--- Ejes.py
+++ Ejes.py
@@ -1,5 +1,5 @@
 
-# Librerias
+# Librerías
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
--- Graficas.py
+++ Graficas.py
@@ -1,5 +1,5 @@
 
-# Librerias
+# Librerías
 
 import matplotlib.pyplot as plt
 import numpy as np
